0dcb819c9891442348b119c8c080e5d8684762ce
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2021-08-18 13:54:06 -0700

    Added CRUD for game object. That included the controller and views.

---
+++ GameManager/Controllers/GamesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GameManager.Models;
+
+namespace GameManager.Controllers
+{
+    public class GamesController : Controller
+    {
+        private GameTrackerContext db = new GameTrackerContext();
+
+        // GET: Games
+        public ActionResult Index()
+        {
+            var games = db.Games.Include(g => g.GameSystem).Include(g => g.GameUser);
+            return View(games.ToList());
+        }
+
+        // GET: Games/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public ActionResult Create()
+        {
+            ViewBag.SystemName = new SelectList(db.GameSystems, "Name", "Name");
+            ViewBag.UserId = new SelectList(db.GameUsers, "UserId", "AspNetUserId");
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserId,Title,Price,SystemName,DateOfPurchase,DatePlayed,Borrowed,Physical,Replayed")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Games.Add(game);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SystemName = new SelectList(db.GameSystems, "Name", "Name", game.SystemName);
+            ViewBag.UserId = new SelectList(db.GameUsers, "UserId", "AspNetUserId", game.UserId);
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SystemName = new SelectList(db.GameSystems, "Name", "Name", game.SystemName);
+            ViewBag.UserId = new SelectList(db.GameUsers, "UserId", "AspNetUserId", game.UserId);
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserId,Title,Price,SystemName,DateOfPurchase,DatePlayed,Borrowed,Physical,Replayed")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(game).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SystemName = new SelectList(db.GameSystems, "Name", "Name", game.SystemName);
+            ViewBag.UserId = new SelectList(db.GameUsers, "UserId", "AspNetUserId", game.UserId);
+            return View(game);
+        }
+
+        // GET: Games/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Game game = db.Games.Find(id);
+            db.Games.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
