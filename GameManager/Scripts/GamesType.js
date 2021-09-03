9b26bc3a1899271da5281c8657fa4586b554987b
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2021-09-03 14:35:25 -0700

    Added GamesType.js.

---
+++ GameManager/Scripts/GamesType.js
@@ -0,0 +1,52 @@
+﻿$("form[name=type_Form]").submit(function (e) {
+    
+    console.log("Form Submited to js");
+    var $form = $(this);
+    var data = $form.serialize();
+    
+    $.ajax({
+        url: "/Games/GameTypeList",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: AddGamesList,
+        error: DetectError
+        
+    });
+    
+
+    e.preventDefault();
+});
+
+// Add table with some game data to page.
+function AddGamesList(data) {
+    $("#searchType").hide();
+    
+
+    var title = data['Title']
+    var titleList = data['TitleList']
+    var dateList = data['DateList']
+    var systemList = data['SystemList']
+
+    $("#tableTitle").text(title + " List");
+
+    for (i = 0; i < titleList.length; i++) {
+        var dataRow = $('<tr>');
+        dataRow.append("<td>" + titleList[i] + "</td>");
+        dataRow.append("<td>" + dateList[i] + "</td>");
+        dataRow.append("<td>" + systemList[i] + "</td>");
+        
+        $("#displayType").append(dataRow);
+    }
+
+    $("#typeTable").show();
+}
+
+function DetectError() {
+    console.log("error");
+}
+
+function ShowInput() {
+    $("#typeTable").hide();
+    $("#searchType").show();
+}
