cf4e6b48c3fac04aeafd5cb32e11ddd82395c9a6
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2021-09-16 13:19:18 -0700

    Maded slight change to GraphSetup.js and related changes else where.

--- GameManager/Scripts/GraphSetup.js
+++ GameManager/Scripts/GraphSetup.js
@@ -1,11 +1,11 @@
-﻿function BarGraphSetup(graphTitle, xValues, yValues) {
+﻿function BarGraphSetup(canvasId,graphTitle, xValues, yValues) {
     var g1 = '#38ef7d';
     var g2 = '#11998e';
     var g3 = '#05665b';
     var color1 = [g1, g2, g1, g2, g1, g2, g1, g2, g1, g2, g1, g2];
     var color2 = [g3, g3, g3, g3, g3, g3, g3, g3, g3, g3, g3, g3,];
 
-    new Chart("myChart", {
+    new Chart(canvasId, {
         type: "bar",
         data: {
             labels: xValues,
@@ -33,13 +33,13 @@
         }
     });
 }
-function PieGraphSetup(graphTitle, xValues, yValues) {
+function PieGraphSetup(canvasId, graphTitle, xValues, yValues) {
     var g3 = '#05665b';
     var color1 = ['#38ef7d', '#18AD94', '#12E8DD', '#2E96E6', '#315AA6', '#656EE6', '#7DE843', '#6ADB00', '#63C757', '#59A6CF' ];
     var color2 = [g3, g3, g3, g3, g3, g3, g3, g3, g3, g3, g3, g3,];
 
 
-    new Chart("myChart", {
+    new Chart(canvasId, {
         type: "pie",
         data: {
             labels: xValues,
